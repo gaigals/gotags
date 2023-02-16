3754f9cc56eb91c0ca0a535be7d6971c8407f685
Kārlis Gaigals <karlis@nic.lv>
2023-02-16 15:54:33 +0200

    Formatting

--- tag_data.go
+++ tag_data.go
@@ -1,50 +1,56 @@
 package gotags
 
 import (
-    "errors"
-    "fmt"
-    "strings"
+	"errors"
+	"fmt"
+	"strings"
 )
 
 type TagData struct {
-    Key   string
-    Value string
+	Key   string
+	Value string
 }
 
 func newTagData(tag, equals string) (*TagData, error) {
-    splitted := strings.Split(tag, equals)
-    splittedLen := len(splitted)
+	splitted := strings.Split(tag, equals)
+	splittedLen := len(splitted)
 
-    if splittedLen == 0 {
-        return nil, errors.New("no keys defined")
-    }
+	if splittedLen == 0 {
+		return nil, errors.New("no keys defined")
+	}
 
-    if splittedLen > 2 {
-        return nil, fmt.Errorf("unexpected tag format '%s'", tag)
-    }
+	if splittedLen > 2 {
+		return nil, fmt.Errorf("unexpected tag format '%s'", tag)
+	}
 
-    tagData := TagData{
-        Key: splitted[0],
-    }
+	tagData := TagData{
+		Key: splitted[0],
+	}
 
-    if splittedLen == 2 {
-        tagData.Value = splitted[1]
-    }
+	if splittedLen == 2 {
+		tagData.Value = splitted[1]
+	}
 
-    return &tagData, nil
+	return &tagData, nil
 }
 
 func (td *TagData) validate(key *Key) error {
-    if key.IsBool && td.Value != "" {
-        return fmt.Errorf("tag '%s' does not take any arguments", td.Key)
-    }
-    if !key.IsBool && td.Value == "" {
-        return fmt.Errorf("tag '%s' requires argument", td.Key)
-    }
+	if key.IsBool && td.Value != "" {
+		return fmt.Errorf("tag '%s' does not take any arguments", td.Key)
+	}
+	if !key.IsBool && td.Value == "" {
+		return fmt.Errorf("tag '%s' requires argument", td.Key)
+	}
 
-    if key.Validator == nil {
-        return nil
-    }
+	if key.Validator == nil {
+		return nil
+	}
 
-    return key.Validator(td.Value)
+	return key.Validator(td.Value)
+}
+
+// StringFormatted formats key and value in provided format.
+// For example, format, `%s=%s` will result in `key=value`.
+func (td *TagData) StringFormatted(format string) string {
+	return fmt.Sprintf(format, td.Key, td.Value)
 }
