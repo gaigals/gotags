8e39dde89f7682e28a05a45b9f35ea2c82402909
gaigals <10630202+gaigals@users.noreply.github.com>
2023-03-10 11:11:16 +0200

    Bug fixes, refactored and added README (#3)
    * Key and TagSettings moved to new files
    
    * Comments + formatting
    
    * Formatting
    
    * Comments
    
    * Updated README.md with example
    
    * Updated go version to 1.20
    
    * Added example
    
    * Optimized code + renamed structs
    
    * Renamed files + added tag parser as public func
    
    * Added field ignore + renamed variables
    
    * More renames
    
    * Added RemoveKey and updated README.md
    
    * Bug fix - empty field in slice
    
    * Out of range fix
    
    * Bug fix for ignored fields
    
    ---------
    
    Co-authored-by: Kārlis Gaigals <karlis@nic.lv>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/gaigals/gotags
 
-go 1.19
+go 1.20
---
+++ tag.go
@@ -0,0 +1,57 @@
+package gotags
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Tag struct {
+	Key   string
+	Value string
+}
+
+func NewTagFromString(tagStr, equals string) (Tag, error) {
+	splitted := strings.SplitN(tagStr, equals, 2)
+	splittedLen := len(splitted)
+
+	if splittedLen == 0 {
+		return Tag{}, errors.New("no keys defined")
+	}
+
+	if splittedLen > 2 {
+		return Tag{}, fmt.Errorf("unexpected tagStr format '%s'", tagStr)
+	}
+
+	tag := Tag{
+		Key: splitted[0],
+	}
+
+	// If tagStr has value ...
+	if splittedLen == 2 {
+		tag.Value = splitted[1]
+	}
+
+	return tag, nil
+}
+
+func (td *Tag) validate(key *Key) error {
+	if key.IsBool && td.Value != "" {
+		return fmt.Errorf("tag '%s' does not take any arguments", td.Key)
+	}
+	if !key.IsBool && td.Value == "" {
+		return fmt.Errorf("tag '%s' requires argument", td.Key)
+	}
+
+	if key.Validator == nil {
+		return nil
+	}
+
+	return key.Validator(td.Value)
+}
+
+// StringFormatted formats key and value in provided format.
+// For example, format, `%s=%s` will result in `key=value`.
+func (td *Tag) StringFormatted(format string) string {
+	return fmt.Sprintf(format, td.Key, td.Value)
+}
